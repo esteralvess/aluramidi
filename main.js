158e403d0985c93fc7e69bc35b12044fe73e4f31
Ester Alves <esteralves.s@icloud.com>
2022-12-09 16:27:19 -0300

    -

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSom;
+    const tecla = listaDeTeclas[contador];
+    
+    const instrumento = tecla.classList[1];
+    
+    console.log(instrumento);
+
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_pom');
+    };
 
     contador += 1;
 
